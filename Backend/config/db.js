808ad1cd3f8a7a69da278d90bc1107d43e1dcb9a
RiyaBadapure <riyab2024@gmail.com>
2026-04-19 21:09:12 +0530

    tables ready

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,19 +1,39 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//   host:     'localhost',
+//   user:     'root',
+//   password: 'riya2006',
+//   database: 'pathforge'
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log('Database connection failed!');
+//     console.log('Error code:', err.code);
+//     console.log('Error message:', err.message);
+//   } else {
+//     console.log('MySQL Connected Successfully!');
+//   }
+// });
+
+// module.exports = db;
+
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host:     'localhost',
-  user:     'root',
-  password: 'riya2006',
-  database: 'pathforge'
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.log('Database connection failed!');
-    console.log('Error code:', err.code);
-    console.log('Error message:', err.message);
+    console.error("❌ DB connection failed:", err);
   } else {
-    console.log('MySQL Connected Successfully!');
+    console.log("✅ Connected to Railway MySQL");
   }
 });
 
